51824e3381a24394cd2225751b5c6fd6ccb0076a
Luce Carter <luce@lucyscarter.co.uk>
2021-03-18 21:33:23 +0000

    WIP - Functioning just need to fix some styling for positioning in the overlay

--- BattleBot/wwwroot/js/chat.js
+++ BattleBot/wwwroot/js/chat.js
@@ -2,28 +2,18 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
-//Disable send button until connection is established
-document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
-    var li = document.createElement("li");
-    li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
+    var encodedMsg = user + " says: " + msg;
+    var p = document.createElement("p");
+    p.className = "message";
+    p.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(p);
 });
 
 connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
+   
 }).catch(function (err) {
     return console.error(err.toString());
-});
-
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
-        return console.error(err.toString());
-    });
-    event.preventDefault();
 });
